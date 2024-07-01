480da52c9cf1a44866c3a94ecbd80ea667ef7e34
hotyears <108710360+hotyears@users.noreply.github.com>
2024-07-01 13:35:21 -0400

    Add files via upload

---
+++ webhookspammer.py
@@ -0,0 +1,30 @@
+"""
+Credits: Webhook spammer made by top
+Discord: top6261
+Github: Top
+"""
+print("""
+                                                                                                 
+ _____ _           _        _ _ _     _   _____         _      _____                             
+|   __|_|_____ ___| |___   | | | |___| |_|  |  |___ ___| |_   |   __|___ ___ _____ _____ ___ ___ 
+|__   | |     | . | | -_|  | | | | -_| . |     | . | . | '_|  |__   | . | .'|     |     | -_|  _|
+|_____|_|_|_|_|  _|_|___|  |_____|___|___|__|__|___|___|_,_|  |_____|  _|__,|_|_|_|_|_|_|___|_|  
+              |_|                                                   |_|                          
+                                    Made by top
+                                  Github: hotyears
+""")
+
+#imports
+from dhooks import Webhook
+import time
+
+#prompts
+message = input("What do you want to spam?: ")
+webhookurl = Webhook(input("Enter webhook: "))
+delay = int(input("Enter a delay: "))
+
+#webhook spamming time
+while True:
+    time.sleep(delay)
+    webhookurl.send(message)
+    print("Sent.")
